ca133e80d98c9abbab7f483056738d2dfa6a027d
Venkaiah-12 <nalagatiravindhar@gmail.com>
2020-01-31 16:10:22 +0530

    [Ravindhar]Add.Implementation of Datastructures

---
+++ DataStructure/Quadratic.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FunctionalPrograms
+{
+    class Quadratic
+    {
+        public  void rootsOfEquation()
+        {
+            double a = Utility.userInputs();
+            double b = Utility.userInputs();
+            double c = Utility.userInputs();
+            double determinent = b * b - 4 * a * c;
+            double root1;
+            double root2;
+
+            //roots are real and differnt
+            if (determinent>0)
+            {
+              root1 =(-b+Math.Sqrt(determinent)/2*a);
+              root2 = (-b - Math.Sqrt(determinent) / 2 * a);
+                Console.WriteLine("the root1 and root2 are: " + root1 + root2);
+
+            }
+            else if(determinent==0)
+                {
+                    root1 = root2 = -b / 2 * a;
+                    Console.WriteLine("the roots of equation are:" + root1);
+
+                }
+            else
+            {
+                double realpart = -b / 2 * a;
+                double imaginarypart = Math.Sqrt(determinent / 2 * a);
+                Console.WriteLine("the realpart and imaginary part:" + realpart + " " +imaginarypart);
+            }
+
+            
+            
+
+        }
+    }
+}
---
+++ DataStructure/UnOrderedList.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.IO;
+
+
+namespace Datastructure
+{
+  
+    class UnOrderedList
+    {
+        string[] arr;
+        public   void unorderedlist()
+        {
+           
+            LinkedList linkedList = new LinkedList();
+            try
+            {
+                
+                
+                StreamReader sr;
+                sr = new StreamReader(@"C:\files\Reading.txt");
+                string str;
+                if((str=sr.ReadLine()) != null)
+                {
+                    arr = str.Split(" ");
+                    for (int i = 0; i < arr.Length; i++)
+                    {
+                        Console.WriteLine(arr[i]);
+                    }
+                  
+
+
+                }
+                Console.WriteLine("nodes of singlylinkedlist");
+                foreach (string strvalues in arr)
+                {
+                    linkedList.addNode(strvalues);
+                }
+               /*Console.WriteLine("the items to be deleted");
+              string item = Utility.userstringinput();
+               linkedList.removeNode(item);
+               Console.WriteLine("enter the item to search  :");
+                string values = Utility.userstringinput();
+                Console.WriteLine(linkedList.Search(item));
+                Console.WriteLine(linkedList.isEmpty());
+                Console.WriteLine(linkedList.Size());
+                Console.WriteLine("enter the item to add to the list");
+                string word = Utility.userstringinput();
+                linkedList.Append(word);*/
+                Console.WriteLine("enter the name to find index");
+                string name = Utility.userstringinput();
+                 Console.WriteLine(linkedList.Index(name));
+                Console.WriteLine("enter the position of node to be inserted");
+                int pos = Utility.IntegerInput();
+                Console.WriteLine("enter the name of node to inserted :");
+                string value1 = Utility.userstringinput();
+                linkedList.Insert(pos, value1);
+                linkedList.Pop();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
+    }
+}
